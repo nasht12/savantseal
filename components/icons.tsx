e3156bc53af67fface87973aea687828c76f8ba5
abhinash tummala <abhinash.t12@gmail.com>
2023-10-08 17:45:26 -0700

    updated icon component

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,5 +1,3 @@
-export type Icon = LucideIcon
-
 import {
     AlertTriangle,
     ArrowRight,
@@ -25,9 +23,10 @@ import {
     Twitter,
     User,
     X,
-    type Icon as LucideIcon,
+    type XIcon as LucideIcon,
   } from "lucide-react"
   
+  export type Icon = typeof LucideIcon
   
   export const Icons = {
     logo: Command,
