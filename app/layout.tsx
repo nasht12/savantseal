8c5e6088fbc152a08161212d261643eea94401e1
abhi t <abhinash.t12@gmail.com>
2024-01-08 23:13:53 -0800

    update blogs and header

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,11 +66,9 @@ export default function RootLayout({
                 priority
               /> */}
             </Link>
-            <Link href="/blog" className="hover:text-blue-400">Blog</Link>
-            {/* <Link href="/assistant">AI Assistant</Link>
-            <Link href="/chat">Chat</Link>
-            <Link href="/collegelist">College List</Link> */}
-            <Link href="/sivvy" className="hover:text-blue-400">Sivvy - AI chat Assistant</Link>
+            <Link href="/blog" className="text-blue-400 bg-blue-100 border border-blue-400 p-2 rounded  hover:text-blue-600">Blog</Link>
+            <Link href="/dashboard" className="text-blue-400 bg-blue-100 border border-blue-400 p-2 rounded  hover:text-blue-600">AI Assistant</Link>
+            <Link href="/profile" className="text-blue-400 bg-blue-100 border border-blue-400 p-2 rounded hover:border-blue-600 hover:text-blue-600">Profile</Link>
             <div className="grow" />
             <SignedIn>
               <div className="hidden sm:block">
